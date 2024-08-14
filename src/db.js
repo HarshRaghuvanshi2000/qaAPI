8a04089c81ce54f4ab79036218b4f2de83d58598
Harsh Raghuvanshi <harshraghuwanshi71@gmail.com>
2024-08-14 12:29:17 +0530

    add .env file

--- src/db.js
+++ src/db.js
@@ -1,11 +1,13 @@
+require('dotenv').config(); // Load environment variables from .env file
+
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'quality_assurance_db',
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306 // Optional, in case you specify a different port in .env
 });
 
 db.connect((err) => {
